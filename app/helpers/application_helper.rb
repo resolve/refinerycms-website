7eeb66fc12b160b2368cacab65ac6d2364a5173e
djones <dgjones@gmail.com>
2010-11-26 16:46:52 +1300

    add stylesheet for blog on it's own

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@
 # You can extend refinery with your own functions here and they will likely not get overriden in an update.
 module ApplicationHelper
   include Refinery::ApplicationHelper # leave this line in to include all of Refinery's core helper methods.
-
+  
+  def is_blog?
+    request.path =~ /^\/blog.*$/
+  end
+  
 end
