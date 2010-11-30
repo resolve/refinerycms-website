baf5334937c4b01cc5283d5cd714ab83704f4788
djones <dgjones@gmail.com>
2010-11-30 21:31:25 +1300

    make the guide urls prettier

--- vendor/engines/guides/app/models/guide.rb
+++ vendor/engines/guides/app/models/guide.rb
@@ -1,5 +1,7 @@
 class Guide < ActiveRecord::Base
 
+  has_friendly_id :title, :use_slug => true
+
   acts_as_indexed :fields => [:title, :description, :guide, :author, :category]
   
   validates_presence_of :title
