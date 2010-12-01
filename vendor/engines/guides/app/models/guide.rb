3a0a434fd7ae0e3219b333efbbd3a4dfa3f6f900
Philip Arndt <parndt@gmail.com>
2010-12-01 15:25:12 +1300

    Fixed the statistics, they were never refreshing. Also fixed deprecation warnings (seems they went ignored :-)).

--- vendor/engines/guides/app/models/guide.rb
+++ vendor/engines/guides/app/models/guide.rb
@@ -3,10 +3,10 @@ class Guide < ActiveRecord::Base
   has_friendly_id :title, :use_slug => true
 
   acts_as_indexed :fields => [:title, :description, :guide, :author, :category]
-  
+
   validates_presence_of :title
   validates_uniqueness_of :title
-  
+
   CATEGORIES = ['Getting Started', 'Essentials', 'Customising your Design', 'Extending with Engines', 'Updating Refinery', 'Hosting', 'Translating', 'Tips and Tricks']
-  
+
 end
