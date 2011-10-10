6b956e58ce741341edd76e6184fbf55b5beef120
Philip Arndt <parndt@gmail.com>
2011-10-10 22:53:08 +1300

    use edge_guide_path where appropriate.

--- vendor/engines/guides/config/routes.rb
+++ vendor/engines/guides/config/routes.rb
@@ -1,7 +1,7 @@
 RefinerycmsWebsite::Application.routes.draw do
   resources :guides, :only => [:index, :show]
   match 'edge-guides', :to => 'guides#index', :branch => 'master', :as => 'edge_guides'
-  match 'edge-guides/:id', :to => 'guides#show', :branch => 'master', :as => 'show_edge_guides'
+  match 'edge-guides/:id', :to => 'guides#show', :branch => 'master', :as => 'edge_guide'
 
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :guides do
