1eee18b18220fb857ff9c8128dfb2b971c0e3cc7
Philip Arndt <p@arndt.io>
2014-04-30 16:29:23 -0700

    raw.github.com -> raw.githubusercontent.com

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -9,7 +9,7 @@ class TemplatesController < ActionController::Metal
     if version == 'edge'
       edge_template = template_directory.join('edge.rb')
       unless edge_template.file?
-        Net::HTTP.start("raw.github.com", :use_ssl => true) do |http|
+        Net::HTTP.start("raw.githubusercontent.com", :use_ssl => true) do |http|
           resp = http.get("/refinery/refinerycms/master/templates/refinery/edge.rb")
           open(edge_template, "wb") do |file|
             file.write(resp.body)
@@ -19,7 +19,7 @@ class TemplatesController < ActionController::Metal
       render :file => edge_template, :content_type => 'text/plain', :layout => nil
     elsif version.present?
       unless (version_template = template_directory.join("#{version}.rb")).file?
-        Net::HTTP.start("raw.github.com", :use_ssl => true) do |http|
+        Net::HTTP.start("raw.githubusercontent.com", :use_ssl => true) do |http|
           url = "/refinery/refinerycms/#{version}/templates/refinery/installer.rb"
           Rails.logger.info "Querying '#{url}'"
           resp = http.get(url)
